acc323b26c5a43572cd945d7eefdbb0b893789ec
sholikin1941720140 <sholikinmuhammad1941720140@gmail.com>
2024-08-01 10:02:46 +0700

    add command

--- app/Http/Controllers/RfidController.php
+++ app/Http/Controllers/RfidController.php
@@ -12,17 +12,21 @@ class RfidController extends Controller
 {
     public function store(Request $request)
     {
+        //request uid dari arduino
         $uid = $request->query('uid');
 
+        //cek apakah uid ada
         if (!$uid) {
             return response()->json(['status' => 'error', 'message' => 'UID tidak terdeteksi'], 400);
         }
 
+        //cek apakah uid ada di database
         $user = User::where('uid', $uid)->first();
         if (!$user) {
             return response()->json(['status' => 'error', 'message' => 'Kartu belum terdaftar'], 404);
         }
 
+        //pengambilan nama dan waktu
         $nama = $user->name;
         $created_at = Carbon::now('Asia/Jakarta')->format('Y-m-d H:i:s');
         // $today = Carbon::today();
@@ -31,12 +35,14 @@ class RfidController extends Controller
         //     return response()->json(['status' => 'already_absent', 'message' => $nama . ' sudah absen']);
         // }
 
+        //simpan data absen
         $absen = new Attendance();
         $absen->user_id = $user->id;
         $absen->created_at = $created_at;
         $absen->updated_at = $created_at;
         $absen->save();
 
+        //pesan sukses untuk arduino
         return response()->json(['status' => 'success', 'message' => $nama . ' berhasil absen']);
     }
 }
