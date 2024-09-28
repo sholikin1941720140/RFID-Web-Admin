83fcb57836dae9848c7108d0641f929f3a42e6ef
sholikin1941720140 <sholikinmuhammad1941720140@gmail.com>
2024-09-28 10:27:07 +0700

    ok

--- resources/views/dashboard/data-master/jam/index.blade.php
+++ resources/views/dashboard/data-master/jam/index.blade.php
@@ -17,12 +17,12 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Daftar Ruangan</h1>
+          <h1>Daftar Jam</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
-            <li class="breadcrumb-item active">Ruangan</li>
+            <li class="breadcrumb-item active">Jam</li>
           </ol>
         </div>
       </div>
@@ -44,7 +44,7 @@
             <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                <h3 class="card-title">Daftar data ruangan</h3>
+                <h3 class="card-title">Daftar data jam</h3>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
